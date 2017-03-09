b9d18b7e084ddc3e8fbfac3ae694692aecc9e95e
Bart Niessen <bart.niessen@gmail.com>
2017-03-09 22:46:50 +0100

    And more added to the site..

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
-const tabs = document.getElementById("tab-wrap").children;
+const tabs = document.getElementById("tab-wrap").children,
+    tabActive = "tab-active",
+    articleActive = "article-active";
 
+// What happens when you click a tab..
 function tabClick () {
-    const article = this.getAttribute("data-article"),
-        aClass = "tab-active";
+    const article = this.getAttribute("data-article");
 
     location.hash = article;
 
-    removeClasses(aClass);
-    this.classList.add(aClass);
-}
-
-for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", tabClick);
+    removeClasses(tabActive);
+    this.classList.add(tabActive);
 }
 
 // Simple function that removes classes based on the given class name.
@@ -25,22 +23,67 @@ function removeClasses (classname) {
 
 // Function that runs to check the location hash and enables the according tab.
 function hashParser () {
-    const hash = location.hash.replace("#", ""),
-        aClass = "article-active";
-
-        console.info(hash);
+    const hash = location.hash.replace("#", "");
 
     if (hash) {
         article = document.getElementById(hash);
 
         if (article) {
-            removeClasses(aClass);
-            article.classList.add(aClass);
+            removeClasses(articleActive);
+            article.classList.add(articleActive);
+
+            // And tab..
+            removeClasses(tabActive);
+            tabs[0].parentNode.querySelector(`[data-article="${hash}"]`).classList.add(tabActive);
         }
     }
 }
 
+// Function to calculate age from a data, plus action to take if date is current
+// Date in format DD-MM-YYYY (AND NO OTHER FFS!)
+function getAge (date, onDate) {
+    // Manual parse to avoid inconsistancies with Date.parse implementation.
+    const unparsedArr = date.split("-"),
+        history = new Date(+unparsedArr[2], (+unparsedArr[1] - 1), (+unparsedArr[0] + 1)),
+        today = new Date(),
+        tY = today.getUTCFullYear(),
+        hY = history.getUTCFullYear(),
+        tM = today.getUTCMonth(),
+        hM = history.getUTCMonth(),
+        tD = today.getUTCDate(),
+        hD = history.getUTCDate();
+
+    let age = tY - hY,
+        month = tM - hM;
+
+    if (month < 0 || (month === 0 && (tD < hD))) {
+        age--;
+    }
+
+    if (onDate) {
+        if (tM === hM && tD === hD) {
+            onDate(age);
+        }
+    }
+    return age;
+}
+
+for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener("click", tabClick);
+}
 // Execute as soon as the script is parsed, a hash could be available;
 hashParser();
 // Look for hash changes (tab click);
 window.addEventListener("hashchange", hashParser);
+
+// Sets age specific to my birthday (31-03-1987);
+(function setAge() {
+    const elems = document.getElementsByClassName("set-age"),
+        currentAge = getAge("31-03-1987", (age) => {
+            document.getElementById("header-title").textContent = `FEEST! Ik ben vandaag ${age} geworden!`;
+        });
+
+    for (let i = 0; i < elems.length; i++) {
+        elems[i].textContent = currentAge;
+    }
+})();
