4bcbe8f04a960c392579b4fa81034f94fedc240b
Bart Niessen <bart.niessen@gmail.com>
2017-04-08 20:52:06 +0200

    Continued to work, lots done, small bug remains with the tab click

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
 "use strict";
 
-const tabs = document.getElementById("tab-wrap").children;
-const tabActive = "tab-active";
-const articleActive = "article-active";
+const navElem = document.querySelector("nav");
+const firstArticle = document.getElementById("landing");
 
-// What happens when you click a tab..
-function tabClick () {
-    const article = this.getAttribute("data-article");
-
-    location.hash = article;
-
-    removeClasses(tabActive);
-    this.classList.add(tabActive);
-}
+let isScrolling = false;
+let menuClick = false;
 
 // Simple function that removes classes based on the given class name.
 function removeClasses (classname) {
@@ -23,22 +15,128 @@ function removeClasses (classname) {
     }
 }
 
-// Function that runs to check the location hash and enables the according tab.
-function hashParser () {
-    const hash = location.hash.replace("#", "");
+function hashNoTag (hash) {
+    return hash.replace("#", "");
+}
 
-    if (hash) {
-        const article = document.getElementById(hash);
+function scroller (y) {
+    window.scrollTo(0, Math.floor(y));
+}
 
-        if (article) {
-            removeClasses(articleActive);
-            article.classList.add(articleActive);
+function bBox (elem) {
+    return elem.getBoundingClientRect();
+}
 
-            // And tab..
-            removeClasses(tabActive);
-            tabs[0].parentNode.querySelector(`[data-article="${hash}"]`).classList.add(tabActive);
+// t: current time, b: begin value, c: change in value thus end - begin, d: duration
+function easeOutQuad (t, b, c, d) {
+    return -c *(t/=d)*(t-2) + b;
+}
+
+function scrollToElement (id) {
+    const elementOffset = document.getElementById(id).offsetTop;
+    const currentScroll = window.pageYOffset;
+    const range = 1;
+    const scrollTime = 50;
+    const scrollTo = {
+        start: 0,
+        top: 0,
+        positive: true
+    };
+    const nav = navElem.clientHeight;
+
+    console.info(id, elementOffset);
+
+    let pos;
+    let i = 0;
+
+    function inRange (x, y) {
+        const rangeSmall = y.top - range;
+        const rangeLarge = y.offset + range;
+
+        console.info(y, x);
+
+        // Scroll is positive
+        if (y.positive === true) {
+            // Normal positive version
+            return x >= rangeSmall && x <= rangeLarge;
+        }
+        // Negative version
+        x *= -1;
+        return x >= rangeLarge && x <= rangeSmall;
+    }
+
+    function animateScroll () {
+        scroller(pos);
+        animation();
+    }
+
+    if (currentScroll < elementOffset) {
+        scrollTo.start = (elementOffset - nav) - currentScroll;
+        scrollTo.top = elementOffset - nav;
+        scrollTo.positive = true;
+    } else {
+        scrollTo.start = (currentScroll - (elementOffset - nav)) * -1;
+        scrollTo.top = (elementOffset - nav) * -1;
+        scrollTo.positive = false;
+    }
+
+    // Start the animation.
+    function animation() {
+        //setTimeout(function (){},fps);
+        if (i < scrollTime) {
+            pos = easeOutQuad(i, currentScroll, scrollTo.start, scrollTime);
+            //console.info("position", pos);
+            console.info("inrange", inRange(pos, scrollTo));
+            if (inRange(pos, scrollTo) === false) {
+                window.requestAnimationFrame(animateScroll);
+                i++;
+            } else {
+                // If it's within range set is to stick to the place we want.
+                scroller(scrollTo.top);
+                menuClick = false;
+                return;
+            }
         }
     }
+    animation();
+}
+
+function onScroll () {
+    const nav = bBox(navElem);
+    const header = bBox(firstArticle);
+
+    // Sticky the navigation!
+    if (nav.top <= 0 && header.top <= nav.height) {
+        navElem.classList.add("nav-sticky-top");
+        firstArticle.style.marginTop = `${nav.height}px`;
+    } else {
+        //if (box2.top >= box.height) {
+        navElem.classList.remove("nav-sticky-top");
+        firstArticle.style.marginTop = 0;
+    }
+    isScrolling = false;
+}
+
+function requestScroll () {
+    if (isScrolling === false) {
+        window.requestAnimationFrame(onScroll);
+    }
+    isScrolling = true;
+}
+
+// What happens when you click a tab..
+function tabClick (evt) {
+    const tabActive = "tab-active";
+
+    location.hash = this.hash;
+
+    // Stop default behaviour.
+    evt.preventDefault();
+
+    removeClasses(tabActive);
+    this.classList.add(tabActive);
+
+    scrollToElement(hashNoTag(this.hash));
 }
 
 // Function to calculate age from a data, plus action to take if date is current
@@ -70,22 +168,39 @@ function getAge (date, onDate) {
     return age;
 }
 
-for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", tabClick);
-}
-// Execute as soon as the script is parsed, a hash could be available;
-hashParser();
-// Look for hash changes (tab click);
-window.addEventListener("hashchange", hashParser);
-
-// Sets age specific to my birthday (31-03-1987);
-(function setAge() {
+// initialization functionality, happens once.
+(function initialization () {
+    const baseTabs = document.getElementById("tab-wrap").children;
     const elems = document.getElementsByClassName("set-age");
     const currentAge = getAge("31-03-1987", (age) => {
+            // Sets age specific to my birthday (31-03-1987);
             document.getElementById("header-title").textContent = `FEEST! Ik ben vandaag ${age} geworden!`;
         });
+    const hash = location.hash;
+    const tabActive = "tab-active";
 
+    // Add click events to the tabs.
+    for (let i = 0; i < baseTabs.length; i++) {
+        baseTabs[i].firstChild.addEventListener("click", tabClick);
+    }
+
+    // Sets age specific to my birthday (31-03-1987);
     for (let i = 0; i < elems.length; i++) {
         elems[i].textContent = currentAge;
     }
+
+    // Function that runs to check the location hash and enables the according tab.
+    if (hash) {
+        const tabs = document.getElementById("tab-wrap");
+        const findTab = tabs.querySelector(`[href="${hash}"]`);
+
+        removeClasses(tabActive);
+
+        if (findTab !== null) {
+            findTab.classList.add(tabActive);
+            scrollToElement(hashNoTag(hash));
+        }
+    }
 })();
+
+window.addEventListener("scroll", requestScroll);
